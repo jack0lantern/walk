9bba6c6c0e811d57f1bf4cce46cd10ca785102ef
Sai Yeluru <syeluru96@gmail.com>
2015-09-27 08:46:58 -0500

    minor edits

--- hacktx1/map.js
+++ hacktx1/map.js
@@ -1,14 +1,31 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 16,
-    center: {lat: 30.286, lng: -97.735},
+    zoom: 17,
+    center: {lat: 30.286, lng: -97.737},
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
 
   var walkPathCoords = [
-    {lat: 30.291972, lng: -97.741168},
-    {lat: 30.291899, lng: -97.740028},
-    {lat: 30.291795, lng: -97.738916}
+   {lat: 30.289039, lng: -97.739754}, 
+{lat: 30.289016, lng: -97.739512}, 
+{lat: 30.289017, lng: -97.739249}, 
+{lat: 30.288000, lng: -97.739242}, 
+{lat: 30.287779, lng: -97.739203}, 
+{lat: 30.287436, lng: -97.739238}, 
+{lat: 30.287053, lng: -97.739280}, 
+{lat: 30.286610, lng: -97.738950}, 
+{lat: 30.286236, lng: -97.738859}, 
+{lat: 30.286135, lng: -97.738673}, 
+{lat: 30.286075, lng: -97.738748}, 
+{lat: 30.285659, lng: -97.738800}, 
+{lat: 30.285460, lng: -97.738824}, 
+{lat: 30.285070, lng: -97.738859}, 
+{lat: 30.285056, lng: -97.738771}, 
+{lat: 30.285011, lng: -97.738769}, 
+{lat: 30.284996, lng: -97.738688}, 
+{lat: 30.284926, lng: -97.738693}, 
+{lat: 30.284743, lng: -97.738500}, 
+{lat: 30.284528, lng: -97.738296}
   ];
   var walkPath = new google.maps.Polyline({
     path: walkPathCoords,
@@ -23,3 +40,14 @@ function initMap() {
  
 //displays map on page load
 google.maps.event.addDomListener(window, 'load', initMap);
+
+
+$(document).ready(function() {
+  $("#submit-button").click(function() {
+  // Instead of directly editing CSS, toggle a class
+    $('.form').fadeOut('slow');
+    $(map).css("opacity", 10);
+  });
+});
+
+
