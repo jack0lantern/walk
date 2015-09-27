79a6217ac3d1d357088db88417fa8dfcb9b58ed3
Jack Jiang <j_j_jiang@yahoo.com>
2015-09-26 22:33:59 -0500

    removed key, added more structure

--- main.js
+++ main.js
@@ -1,29 +1,5 @@
-var sp = require('excel');
-var fs = require('fs');
-var result = [];
-var x = sp('list3.xlsx', function(err, data) {
-	if(err) throw err;
-	console.log(data[0]);
-});
-/*var csv = require('csv-parser')
- 
-fs.createReadStream('list.csv')
-  .pipe(csv())
-  .on('data', function(data) {
-	  console.log("print something"); 
-    console.log('row', data)
-  })
-console.log(sp);*/
-var XLSX = require('xlsx');
-var workbook = XLSX.readFile('list3.xlsx');
-//console.log("%j", workbook);
+var http = require('http');
+var graph = require('./graph');
+var map = require('./map');
 
-var sheet_name_list = workbook.SheetNames;
-sheet_name_list.forEach(function(y) { /* iterate through sheets */
-  var worksheet = workbook.Sheets[y];
-  for (var z in worksheet) {
-    /* all keys that do not begin with "!" correspond to cell addresses */
-    if(z[0] === '!') continue;
-    console.log(worksheet[z].v);
-  }
-});
+map.build('list.xlsx');
---
+++ map.js
@@ -0,0 +1,16 @@
+var XLSX = require('xlsx');
+
+
+exports.build = function(sheet) {
+  var workbook = XLSX.readFile(sheet);
+  var sheet_name_list = workbook.SheetNames;
+  sheet_name_list.forEach(function(y) { /* iterate through sheets */
+    var worksheet = workbook.Sheets[y];
+    for (var z in worksheet) {
+      /* all keys that do not begin with "!" correspond to cell addresses */
+      if(z[0] === '!') continue;
+      console.log(worksheet[z].v);
+    }
+  });
+};
+
